c7cb20f34eea10308abcab18747ff29fd27de26c
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-05-08 01:51:20 +0530

    getting concurrent connections

---
+++ TCP_net_module_NodeJS/getConnections.js
@@ -0,0 +1,25 @@
+var net = require('net')
+
+var tcpServer = net.createServer();
+
+tcpServer.on('connection', function (socket) {
+
+    console.log('a client connected to server');
+
+    this.getConnections((error, count) => {
+        console.log(`Number of connections : ${count}`);
+    })
+
+    socket.on('data', (data) => {
+        console.log(`Received data: ${data}`);
+        socket.write(`server reply: ${data}`)
+        // socket.end()
+    });
+
+    socket.on('end', () => {
+        console.log('Client disconnected');
+    });
+
+}).listen(3000, () => {
+    console.log("tcp server listening at port 3000");
+})
