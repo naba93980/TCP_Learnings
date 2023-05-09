0f16222a5eaee29418ab73896f960dcf0387ac0c
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-05-10 00:11:57 +0530

    added socketStats bytes fn

--- TCP_net_module_NodeJS/utils/socketUtils.js
+++ TCP_net_module_NodeJS/utils/socketUtils.js
@@ -1,7 +1,14 @@
-function socketUtil(socket) {
+function socketAddress(socket) {
     console.log("remotePort : ", socket.remotePort);
     console.log("remoteAddress : ", socket.remoteAddress);
 }
+
+function socketStats(socket) {
+    console.log("bytes read : ", socket.bytesRead);
+    console.log("bytes written : ", socket.bytesRead);
+}
+
 module.exports = {
-    socketUtil
+    socketAddress,
+    socketStats
 }
