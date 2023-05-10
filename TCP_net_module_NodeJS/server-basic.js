d0567fa03704f956570ba0899839e309e8629836
naba93980 <72063612+naba93980@users.noreply.github.com>
2023-05-11 00:45:59 +0530

    half open socket

--- TCP_net_module_NodeJS/server-basic.js
+++ TCP_net_module_NodeJS/server-basic.js
@@ -1,6 +1,8 @@
 var net = require('net')
 
-var tcpServer = net.createServer();
+var tcpServer = net.createServer({
+    allowHalfOpen: true                         // socket will be closed only when socket.end() is called
+});
 
 
 // When a client connects to the server, the server's net.Server emits a 'connection' event, passing a new Socket instance representing the new connection as an argument to the event handler function
